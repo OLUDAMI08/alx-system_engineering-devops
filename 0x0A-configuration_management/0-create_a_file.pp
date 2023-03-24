2dca5b043b7f96993e9eb37d6314b78a0cd8dbc8
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-24 19:07:28 +0000

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This manifest creates a file at /tmp
-
 file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  ensure => file,
+  content => 'I love Puppet',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
 }
+
